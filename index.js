0b9724240828188c2a1866144b779020dabebaa1
aethiu <147225680+aethiu@users.noreply.github.com>
2024-05-26 22:30:57 -0700

    Nest item list into component Move PHP backend into model/ directory

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 async function getItem(sku) {
-    const response = await fetch("get_items.php?sku="+sku);
+    const response = await fetch("model/get_items.php?sku="+sku);
     const items = await response.json();
-    return items[0];
+    return items && items[0];
 }
 
-function ItemCard({sku}) {
-    const [item, setItem] = React.useState();
+async function getItems() {
+    const response = await fetch("model/get_items.php");
+    return await response.json();
+}
 
-    React.useEffect(() => {
-        let ignore = false;
-        async function startFetch() {
-            const res = await getItem(sku);
-            if (!ignore) {
-                setItem(res);
-            }
-        }
-        startFetch();
-        return () => { ignore = true; };
-    }, [sku]);
-
+function ItemCard({item}) {
     return (
         <div className="item_card">
             <div className="item_image_placeholder"></div>
@@ -30,8 +21,24 @@ function ItemCard({sku}) {
 }
 
 function ItemList() {
-    const skus = [1, 2, 3, 4, 5];
-    const items = skus.map(sku => <ItemCard sku={sku} key={sku}/>);
+    const [items, setItems] = React.useState();
+
+    React.useEffect(() => {
+        let ignore = false;
+
+        async function startFetch() {
+            const res = await getItems();
+            if (!ignore) {
+                const itemlist = res.map(item => <ItemCard item={item} key={item.sku}/>);
+                setItems(itemlist);                
+            }
+        }
+        startFetch();
+        
+        return () => { ignore = true; };
+
+    }, []);
+
     return (
         <div className="items">{items}</div>
     );
