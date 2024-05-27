fa4195de8fe4bf2a8aa2defeb921635379b997a9
aethiu <147225680+aethiu@users.noreply.github.com>
2024-05-27 11:52:21 -0700

    Add login page

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <bodY>
+        <form>
+            <label for="username">Username</label>
+            <input type="text" name="username"></input>
+            <br/>
+            <label for="password">Password</label>
+            <input type="password" name="password"></input>
+            <br/>
+            <button>Login</button>
+            <br/>
+            <button type="button">Cancel</button>
+        </form>
+    </bodY>
+</html>
