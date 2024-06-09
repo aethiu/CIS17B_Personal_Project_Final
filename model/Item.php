3cbe59639dbbdae0eecc75ac621d5ce9bb944eed
aethiu <147225680+aethiu@users.noreply.github.com>
2024-06-08 23:06:37 -0700

    Add query function to PHP item model

--- model/Item.php
+++ model/Item.php
@@ -1,4 +1,5 @@
 <?php
+
 class Item {
     public $sku;
     public $price;
@@ -6,4 +7,20 @@ class Item {
     public $name;
     public $description;
     const table = "entity_item";
+
+    public static function get($conn, $sku=null) {
+        $table = Item::table;
+        $sql = "SELECT
+            {$table}.sku,
+            {$table}.price,
+            {$table}.quantity,
+            {$table}.name,
+            {$table}.description
+            FROM {$table}";
+        if ($sku) {
+             $sql."WHERE sku=$sku";
+        } 
+        $items = fetchClass($conn, $sql, "Item");
+        return $sku ? $items[0] : $items;
+    }
 }
